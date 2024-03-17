e5b3160c63899bec5cadf9be695b82b1ec9ed2c9
Nikita Ambalov <js.python.st@gmail.com>
2024-03-17 20:56:53 +0300

    committed the finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { buttons } from "./buttons-data";
 
-function App() {
+const App = () => {
+  const [firstOperand, setFirstOperand] = useState("");
+  const [secondOperand, setSecondOperand] = useState("");
+  const [operator, setOperator] = useState("");
+  const [flag, setFlag] = useState(false);
+
+  const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+  const displayDigits = (e) => {
+    if (operator === "") {
+      console.log(firstOperand, typeof firstOperand);
+
+      setFirstOperand(firstOperand.toString().concat(e.target.textContent));
+    }
+    if (operator !== "") {
+      setSecondOperand(secondOperand.concat(e.target.textContent));
+    }
+  };
+
+  const addNumbers = () => {
+    if (flag === true) {
+      setFlag(false);
+    }
+    setOperator("+");
+  };
+
+  const deductNumbers = () => {
+    if (flag === true) {
+      setFlag(false);
+    }
+    setOperator("-");
+  };
+
+  function reset() {
+    setFirstOperand("");
+    setSecondOperand("");
+    setOperator("");
+    setFlag(false);
+  }
+
+  let result;
+
+  const calculateResult = () => {
+    if (operator === "+") {
+      result = Number(firstOperand) + Number(secondOperand);
+    }
+    if (operator === "-") {
+      result = Number(firstOperand) - Number(secondOperand);
+    }
+
+    setFlag(!flag);
+    setOperator("");
+    setFirstOperand(result);
+    setSecondOperand("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className={flag ? "result output" : "result-output output"}>
+        {result
+          ? `(${result})`
+          : `${firstOperand} ${operator} ${secondOperand}`}
+      </div>
+      <div className="calculator__buttons">
+        <button className="button-c" onClick={reset}>
+          C
+        </button>
+        <button onClick={addNumbers}>+</button>
+        <button onClick={deductNumbers}>-</button>
+
+        {nums.map((num, idx) => {
+          return (
+            <button key={idx} onClick={displayDigits}>
+              {num}
+            </button>
+          );
+        })}
+
+        <button className="button-equal" onClick={calculateResult}>
+          =
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/buttons-data.js
@@ -0,0 +1,14 @@
+export const buttons = [
+  { id: "11", name: "-", text: "-" },
+  { id: "12", name: "+", text: "+" },
+  { id: "1", name: "1", text: "1" },
+  { id: "2", name: "2", text: "2" },
+  { id: "3", name: "3", text: "3" },
+  { id: "4", name: "4", text: "4" },
+  { id: "5", name: "5", text: "5" },
+  { id: "6", name: "6", text: "6" },
+  { id: "7", name: "7", text: "7" },
+  { id: "8", name: "8", text: "8" },
+  { id: "9", name: "9", text: "9" },
+  { id: "10", name: "0", text: "0" },
+];
